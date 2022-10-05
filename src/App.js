b622abaf3431547588566fce41685a15c4553ba7
Mohamed Sawah <sawah810@gmail.com>
2022-10-05 20:52:57 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {Col, Container, Row } from "react-bootstrap";
 import person from "./data";
 import DateItem from "./components/DateItem";
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 function App() {
   const [personData,setPersonData]=useState(person);
-  
+  useEffect(()=>{
+    setPersonData([])
+  },[])
 
   return (
     <div className="App font color-body">
@@ -28,7 +30,7 @@ function App() {
         <Row className="justify-content-center my-2">
           <Col sm="8" className="d-flex justify-content-between">
             <button onClick={()=>{setPersonData([])}} className="btn-style p-2">مسح الكل</button>
-            <button onClick={()=>{setPersonData(person)}} className="btn-style p-2">عرض الكل</button>
+            <button onClick={()=>{setPersonData(person)}} className="btn-style p-2">عرض البيانات</button>
           </Col>
         </Row>
       </Container>
